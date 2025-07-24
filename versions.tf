2db5c9c16beeaebf317b668597387b70ab5b38aa
MauroSoli <mauro.solidoro@outlook.it>
2025-07-24 16:49:13 +0200

    feat: upgrade terraform versions

--- versions.tf
+++ versions.tf
@@ -1,11 +1,11 @@
 # versions.tf
 
 terraform {
-  required_version = ">= 1.2.9"
+  required_version = ">= 1.10.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.41"
+      version = "~> 6.4.0"
     }
   }
 }
